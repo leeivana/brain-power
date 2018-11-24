4220f978aaad87f3c2380d67dc956e2ca381db7f
Gavin Ching <gavinchingy@gmail.com>
2018-11-23 20:40:02 -0800

    Add in route for Student

--- client/modules/Student/Student.js
+++ client/modules/Student/Student.js
@@ -2,13 +2,20 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
+import Button from '@material-ui/core/Button';
 
 // Import Style
 import styles from './Student.css';
 
 class Student extends Component {
   render() {
-    return <div />;
+    return (
+      <div>
+        <Button variant="contained" color="primary">
+          Hello World
+        </Button>
+      </div>
+    );
   }
 }
 
--- client/routes.js
+++ client/routes.js
@@ -39,5 +39,13 @@ export default (
         });
       }}
     />
+    <Route
+      path="/students"
+      getComponent={(nextState, cb) => {
+        require.ensure([], require => {
+          cb(null, require('./modules/Student/Student').default);
+        });
+      }}
+    />
   </Route>
 );
