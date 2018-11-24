a4e6a04d4d523dda1724ac7fc81ab56a7efde2d4
leeivana <lee.ivana@hotmail.com>
2018-11-23 23:13:37 -0500

    Administrator schema

---
+++ client/modules/Administrator/Administrator.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+// Import Style
+import styles from './Administrator.css';
+
+class Administrator extends Component {
+  render() {
+    return (
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {};
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {};
+};
+
+Administrator.propTypes = {
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Administrator);
